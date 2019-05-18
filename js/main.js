143a40549d2607690f30810e0ca17ace8cdcbc9c
watepu <40508321+watepu@users.noreply.github.com>
2019-05-18 13:56:12 +0900

    現在の時刻を取得

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+{
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const result = document.getElementById('result');
+  let startTime;
+
+  start.addEventListener('click', function(){
+    startTime = Date.now();
+
+  });
+
+  stop.addEventListener('click', function(){
+
+  });
+}
