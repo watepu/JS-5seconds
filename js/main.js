8c6e96fcb678f9a58331ebf60eea3274ba6efcb8
watepu <40508321+watepu@users.noreply.github.com>
2019-05-18 14:03:28 +0900

    stopボタンを押した際時間を表示するよう設定

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@
 
   start.addEventListener('click', function(){
     startTime = Date.now();
-
   });
 
   stop.addEventListener('click', function(){
-
+    let elapsedTime = (Date.now() - startTime) / 1000;
+    result.textContent = elapsedTime.toFixed(3);
+    // toFixed(3)で小数点第3位まで表示するよう設定
   });
 }
