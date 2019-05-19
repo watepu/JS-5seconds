85db772839f33954e0b6d09ccf5101e39dd422b6
watepu <40508321+watepu@users.noreply.github.com>
2019-05-19 09:02:54 +0900

    ボタンを押し込めるようなスタイルを設定

--- js/main.js
+++ js/main.js
@@ -7,11 +7,15 @@
 
   start.addEventListener('click', function(){
     startTime = Date.now();
+    this.className = 'pushed';
+    stop.className = '';
   });
 
   stop.addEventListener('click', function(){
     let elapsedTime = (Date.now() - startTime) / 1000;
     result.textContent = elapsedTime.toFixed(3);
+    this.className = 'pushed';
+    start.className = '';
     // toFixed(3)で小数点第3位まで表示するよう設定
     let diff = elapsedTime - 5;
     if(Math.abs(diff) < 1.0){
